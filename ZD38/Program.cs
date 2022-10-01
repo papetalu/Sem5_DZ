07174fad1d6a8efdf6b3eb6e9b0821864cf986d0
Liuba <pl@mail.ru>
2022-10-01 13:31:58 +0300

    Task 3

--- ZD38/Program.cs
+++ ZD38/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+
+void InputArray(double[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().NextDouble() * 10;
+}
+
+double MaxArray(double[] array)
+{
+   double max = array[0];
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] >= max)
+         max = array[i];
+   }
+   return max;
+}
+
+double MinArray(double[] array)
+{
+   double min = array[0];
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] <= min)
+         min = array[i];
+   }
+   return min;
+}
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.Write("Максимальное число: ");
+Console.WriteLine(MaxArray(array));
+Console.Write("Минимальное число: ");
+Console.WriteLine(MinArray(array));
+double dif = MaxArray(array) - MinArray(array);
+
+Console.Write("Разница между максимальным и минимальным числом = " + dif);
