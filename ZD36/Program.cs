9af72d2033c1cffb9d8c7298cddb9ffcd112fb25
Liuba <pl@mail.ru>
2022-10-01 12:54:11 +0300

    Task 2

--- ZD36/Program.cs
+++ ZD36/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Clear();
+
+void InputArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(-1000, 1000);
+}
+
+int ReleaseArray(int[] array)
+{
+   int sum = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (i % 2 != 0)
+         sum = sum + array[i];
+   }
+   return sum;
+}
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine(ReleaseArray(array));
+
